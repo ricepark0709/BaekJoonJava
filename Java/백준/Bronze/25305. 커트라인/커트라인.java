b010a850ca9fe2dc19d11f7cfd2208b8c785b154
ricepark0709 <juncomputer0709@gmail.com>
2026-03-04 09:39:13 +0900

    [Bronze II] Title: 커트라인, Time: 260 ms, Memory: 20968 KB -BaekjoonHub

---
+++ Java/백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int inputNum = sc.nextInt();
+            arr[i] = inputNum;
+        }
+        
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        
+        System.out.println(arr[k-1]);
+    }
+}
