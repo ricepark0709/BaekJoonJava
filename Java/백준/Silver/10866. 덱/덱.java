5c5392bf8321c9fc735d58a9134a990e2c8ec5d9
ricepark0709 <juncomputer0709@gmail.com>
2026-03-09 09:47:29 +0900

    [Silver IV] Title: 덱, Time: 168 ms, Memory: 18796 KB -BaekjoonHub

--- Java/백준/Silver/10866. 덱/덱.java
+++ Java/백준/Silver/10866. 덱/덱.java
@@ -1,48 +1,56 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 import java.util.Deque;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> d = new ArrayDeque<>();
         int n = Integer.parseInt(br.readLine());
-        Deque<Integer> deque = new LinkedList<>();
         
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String str = st.nextToken();
             switch (str) {
                 case "push_front":
-                    deque.addFirst(Integer.parseInt(st.nextToken()));
+                    int o = Integer.parseInt(st.nextToken());
+                    d.offerFirst(o);
                     break;
                 case "push_back":
-                    deque.addLast(Integer.parseInt(st.nextToken()));
+                    int b = Integer.parseInt(st.nextToken());
+                    d.offerLast(b);
                     break;
                 case "pop_front":
-                    if (deque.isEmpty()) System.out.println(-1);
-                    else System.out.println(deque.pollFirst());
+                    sb.append((d.isEmpty()) ? -1 : d.pollFirst()).append("\n");
                     break;
                 case "pop_back":
-                    if (deque.isEmpty()) System.out.println(-1);
-                    else System.out.println(deque.pollLast());
+                    sb.append((d.isEmpty()) ? -1 : d.pollLast()).append("\n");
                     break;
                 case "size":
-                    System.out.println(deque.size());
+                    sb.append(d.size()).append("\n");
                     break;
                 case "empty":
-                    if (deque.isEmpty()) System.out.println(1);
-                    else System.out.println(0);
+                    sb.append((d.isEmpty()) ? 1 : 0).append("\n");
                     break;
                 case "front":
-                    if (deque.isEmpty()) System.out.println(-1);
-                    else System.out.println(deque.peekFirst());
+                    sb.append((d.isEmpty()) ? -1 : d.peekFirst()).append("\n");
                     break;
                 case "back":
-                    if (deque.isEmpty()) System.out.println(-1);
-                    else System.out.println(deque.peekLast());
+                    sb.append((d.isEmpty()) ? -1 : d.peekLast()).append("\n");
                     break;
             }
         }
+        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        
+        bw.flush();
+        bw.close();
     }
 }
