c061ff437efb6aa9145765cb853d4ca70bd034a4
ricepark0709 <juncomputer0709@gmail.com>
2026-03-10 02:13:46 +0900

    [Silver II] Title: 최소 힙, Time: 308 ms, Memory: 27484 KB -BaekjoonHub

---
+++ Java/백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Long> q = new PriorityQueue<>();
+
+        int zero = 0;
+        for (int i = 0; i < n; i++) {
+            long a = Long.parseLong(br.readLine());
+            if (!(a==0)) q.add(a);
+            else if (a==0 && !q.isEmpty()) sb.append(q.poll() + "\n");
+            else if (a==0 && q.isEmpty()) sb.append(0 + "\n");
+        }
+
+        for (int i = 0; i < zero; i++) {
+            if (!q.isEmpty()) sb.append(q.poll() + "\n");
+            else sb.append(0 + "\n");
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+    }
+}
