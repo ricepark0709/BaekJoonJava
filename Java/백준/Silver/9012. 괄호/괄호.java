72107c3ff4dda3946c7abdc1db274f876fe058b7
ricepark0709 <juncomputer0709@gmail.com>
2026-03-09 20:38:20 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14388 KB -BaekjoonHub

--- Java/백준/Silver/9012. 괄호/괄호.java
+++ Java/백준/Silver/9012. 괄호/괄호.java
@@ -1,43 +1,38 @@
-import java.util.Stack;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-public class Main { // 클래스 명은 반드시 Main!
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // n 입력 시 공백이 섞일 수 있으므로 trim() 사용
-        String inputN = br.readLine();
-        if (inputN == null) return;
-        int n = Integer.parseInt(inputN.trim());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
         
         for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            if (line == null) break;
-
+            String str = br.readLine();
             Stack<Character> s = new Stack<>();
-            boolean isGood = true; 
-
-            for (char c : line.toCharArray()) {
-                if (c == '(') {
-                    s.push(c);
-                } else {
-                    // ')' 가 나왔는데 스택이 비어있으면 실패!
+            boolean isBroken = false;
+            
+            for (char c : str.toCharArray()) {
+                if (c == '(') s.push(c);
+                else {
                     if (s.isEmpty()) {
-                        isGood = false;
-                        break; 
+                        sb.append("NO\n");
+                        isBroken = true;
+                        break;
                     }
                     s.pop();
                 }
             }
-
-            // 다 끝났는데 스택에 '(' 가 남아있어도 안됨!
-            if (isGood && s.isEmpty()) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
+            if (!isBroken) {
+                if (s.isEmpty()) sb.append("YES\n");
+                else sb.append("NO\n");
             }
         }
+        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        
+        bw.flush();
+        bw.close();
     }
 }
