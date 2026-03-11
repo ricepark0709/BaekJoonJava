d0a19c96470aebcc1670e492a2ffbefa7916739d
ricepark0709 <juncomputer0709@gmail.com>
2026-03-11 13:58:50 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 108 ms, Memory: 14448 KB -BaekjoonHub

---
+++ Java/백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int expected = 1;
+
+        for (int i = 0; i < n; i++) {
+            int current = Integer.parseInt(st.nextToken());
+
+            if (current == expected) expected++;
+            else stack.push(current);
+
+            while (!stack.isEmpty() && stack.peek() == expected) {
+                stack.pop();
+                expected++;
+            }
+        }
+
+        if (stack.isEmpty()) {
+            System.out.println("Nice");
+        } else {
+            System.out.println("Sad");
+        }
+    }
+}
