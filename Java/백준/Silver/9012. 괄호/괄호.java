96d4e4e52be452bd3b55e2734ae826f7829ad366
ricepark0709 <juncomputer0709@gmail.com>
2026-03-16 05:37:01 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14336 KB -BaekjoonHub

--- Java/백준/Silver/9012. 괄호/괄호.java
+++ Java/백준/Silver/9012. 괄호/괄호.java
@@ -2,37 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        
+
         for (int i = 0; i < n; i++) {
-            String str = br.readLine();
             Stack<Character> s = new Stack<>();
-            boolean isBroken = false;
-            
-            for (char c : str.toCharArray()) {
-                if (c == '(') s.push(c);
-                else {
+            String str = br.readLine();
+            boolean isError = false; // 중간에 잘못된 경우를 체크
+
+            for (int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (c == '(') {
+                    s.push(c);
+                } else { // ')' 인 경우
                     if (s.isEmpty()) {
-                        sb.append("NO\n");
-                        isBroken = true;
-                        break;
+                        isError = true;
+                        break; // for 루프를 완전히 빠져나감
                     }
                     s.pop();
                 }
             }
-            if (!isBroken) {
-                if (s.isEmpty()) sb.append("YES\n");
-                else sb.append("NO\n");
+
+            // 1. 중간에 에러가 났거나(isError) 
+            // 2. 다 끝났는데 스택에 '('가 남아있으면 NO
+            if (isError || !s.isEmpty()) {
+                sb.append("NO\n");
+            } else {
+                sb.append("YES\n");
             }
         }
-        
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(sb.toString());
-        
-        bw.flush();
-        bw.close();
+
+        System.out.print(sb);
     }
 }
