d7bfe7c7c6a647c265ddefbdb330ee717c3af4c4
ricepark0709 <juncomputer0709@gmail.com>
2026-03-18 23:51:29 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- Java/백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ Java/백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -16,38 +16,7 @@ public class Main {
         String str = Integer.toString(sum);
         
         for (int i = 0; i < str.length(); i++) {
-            switch (str.charAt(i)) {
-                case '0':
-                    arr[0]++;
-                    break;
-                case '1':
-                    arr[1]++;
-                    break;
-                case '2':
-                    arr[2]++;
-                    break;
-                case '3':
-                    arr[3]++;
-                    break;
-                case '4':
-                    arr[4]++;
-                    break;
-                case '5':
-                    arr[5]++;
-                    break;
-                case '6':
-                    arr[6]++;
-                    break;
-                case '7':
-                    arr[7]++;
-                    break;
-                case '8':
-                    arr[8]++;
-                    break;
-                case '9':
-                    arr[9]++;
-                    break;
-            }
+            arr[str.charAt(i) - '0']++;
         }
         
         for (int i : arr) {
