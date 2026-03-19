69bcf50a2df1b69b8da73c77f9760796dd8ea03a
ricepark0709 <juncomputer0709@gmail.com>
2026-03-19 18:32:01 +0900

    [Bronze III] Title: 웰컴 키트, Time: 116 ms, Memory: 15744 KB -BaekjoonHub

--- Java/백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ Java/백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -2,34 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr = new int[6];
+    static int N, T, P, TBox;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int total = Integer.parseInt(br.readLine());
-        int[] arr = new int[6];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        for (int i = 0; i < arr.length; i++) {
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 6; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         st = new StringTokenizer(br.readLine());
-        int t = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
-        
-        int totalT = 0;
-        for (int i = 0; i < 6; i++) {
-            if (arr[i] > 0) {
-                totalT += (arr[i] + t - 1) / t;
-            }
+        T = Integer.parseInt(st.nextToken());
+        P = Integer.parseInt(st.nextToken());
+
+        for (int i : arr) {
+            int buy = i/T;
+            if (i%T == 0) TBox += buy;
+            else TBox += (buy+1);
         }
-        
-        sb.append(totalT + "\n").append(total/p).append(" ").append(total%p);
-        
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb.append(TBox + " ").append("\n").append(N/P + " ").append(N%P);
+
         bw.write(sb.toString());
-        
         bw.flush();
         bw.close();
     }
