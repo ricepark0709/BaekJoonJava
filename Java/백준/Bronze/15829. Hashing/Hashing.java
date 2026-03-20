4a785c824eec42131eec7729304291fcc09c2406
ricepark0709 <juncomputer0709@gmail.com>
2026-03-20 11:58:09 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14292 KB, Score: 50 point -BaekjoonHub

--- Java/백준/Bronze/15829. Hashing/Hashing.java
+++ Java/백준/Bronze/15829. Hashing/Hashing.java
@@ -4,21 +4,23 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static long sum;
+    static double to;
     static char[] arr;
-    
+
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        
+
         arr = new char[n];
         for (int i = 0; i < n; i++) {
             arr[i] = str.charAt(i);
         }
-        
+
         for (int i = 0; i < n; i++) {
-            sum += (arr[i] - '`') * Math.pow(31, i);
+            to = Math.pow(31, i);
+            sum += (arr[i] - '`') * to;
         }
-        
+
         System.out.println(sum);
     }
 }
