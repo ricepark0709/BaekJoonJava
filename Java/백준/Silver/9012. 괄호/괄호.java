769a926804f53619c7d694407c7843e2209fff32
ricepark0709 <juncomputer0709@gmail.com>
2026-03-22 16:32:30 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

--- Java/백준/Silver/9012. 괄호/괄호.java
+++ Java/백준/Silver/9012. 괄호/괄호.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
@@ -11,30 +12,31 @@ public class Main {
         for (int i = 0; i < n; i++) {
             Stack<Character> s = new Stack<>();
             String str = br.readLine();
-            boolean isError = false; // 중간에 잘못된 경우를 체크
+            boolean b = true;
 
             for (int j = 0; j < str.length(); j++) {
-                char c = str.charAt(j);
-                if (c == '(') {
-                    s.push(c);
-                } else { // ')' 인 경우
-                    if (s.isEmpty()) {
-                        isError = true;
-                        break; // for 루프를 완전히 빠져나감
-                    }
-                    s.pop();
+                switch (str.charAt(j)) {
+                    case '(':
+                        s.push('(');
+                        break;
+                    case ')':
+                        if (!s.isEmpty()) {
+                            s.pop();
+                        } else {
+                            b = false;
+                            break;
+                        }
+                        break;
                 }
             }
 
-            // 1. 중간에 에러가 났거나(isError) 
-            // 2. 다 끝났는데 스택에 '('가 남아있으면 NO
-            if (isError || !s.isEmpty()) {
-                sb.append("NO\n");
-            } else {
-                sb.append("YES\n");
-            }
+            if (b && s.isEmpty()) sb.append("YES\n");
+            else sb.append("NO\n");
         }
 
-        System.out.print(sb);
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
     }
 }
