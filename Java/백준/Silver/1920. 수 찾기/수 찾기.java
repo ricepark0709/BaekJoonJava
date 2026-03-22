c7b37e856407344ccd49d43084175113e39dc354
ricepark0709 <juncomputer0709@gmail.com>
2026-03-22 14:59:21 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 50004 KB -BaekjoonHub

--- Java/백준/Silver/1920. 수 찾기/수 찾기.java
+++ Java/백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,30 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static Set<Integer> s1 = new HashSet<>();
+    static Set<Integer> s2 = new HashSet<>();
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        Set<Integer> s = new HashSet<>();
-        
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            s.add(Integer.parseInt(st.nextToken()));
+            s1.add(Integer.parseInt(st.nextToken()));
         }
-        
+
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        
         for (int i = 0; i < m; i++) {
-            if (s.contains(Integer.parseInt(st.nextToken()))) sb.append("1\n");
-            else sb.append("0\n");
+            int num = Integer.parseInt(st.nextToken());
+            if (s1.contains(num)) sb.append(1 + "\n");
+            else sb.append(0 + "\n");
         }
-        
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         bw.write(sb.toString());
-        
+
         bw.flush();
         bw.close();
     }
