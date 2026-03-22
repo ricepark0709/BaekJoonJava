b2591f66b996fcf89fc1a85d08071ea890d46a13
ricepark0709 <juncomputer0709@gmail.com>
2026-03-22 16:11:59 +0900

    [Silver IV] Title: solved.ac, Time: 292 ms, Memory: 37952 KB -BaekjoonHub

--- Java/백준/Silver/18110. solved．ac/solved．ac.java
+++ Java/백준/Silver/18110. solved．ac/solved．ac.java
@@ -4,30 +4,25 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int[] arr;
+    static int sum;
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        
-        if (n == 0) {
-            System.out.println(0);
-            return;
-        }
-        
+        int trimN = (int)Math.round(n*0.15);
+        int realN = (int)n-2*trimN;
+
         arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        
+
         Arrays.sort(arr);
-        
-        int trimN = (int) Math.round(n*0.15);
-        
-        double sum = 0;
+
         for (int i = trimN; i < n-trimN; i++) {
             sum += arr[i];
         }
-        
-        int realN = n - 2*trimN;
-        System.out.println((int) Math.round(sum/realN));
+
+        double avg = Math.round((double)sum/(double)realN);
+        System.out.println((int)avg);
     }
 }
