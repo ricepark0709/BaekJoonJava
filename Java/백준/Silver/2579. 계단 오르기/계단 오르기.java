b5ee4145d821a3346c7648199768981b7027ad48
ricepark0709 <juncomputer0709@gmail.com>
2026-03-24 19:34:58 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

--- Java/백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ Java/백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,26 +1,35 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
 
-        int[] score = new int[n + 1];
-        int[] dp = new int[n + 1];
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        setStair(n);
+        dp(n);
+    }
 
+    static int[] stair;
+    public static void setStair(int n) throws IOException {
+        stair = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            score[i] = sc.nextInt();
+            stair[i] = Integer.parseInt(br.readLine());
         }
+    }
 
-        // 초기값 세팅
-        dp[1] = score[1];
-        if (n >= 2) dp[2] = score[1] + score[2];
-
-        // 점화식 진행
-        for (int i = 3; i <= n; i++) {
-            dp[i] = Math.max(dp[i - 2], dp[i - 3] + score[i - 1]) + score[i];
+    public static void dp(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = stair[1];
+        if (n > 1) {
+            dp[2] = stair[1] + stair[2];
+            if (n > 2) {
+                for (int i = 3; i <= n; i++) {
+                    dp[i] = Math.max(dp[i - 2] + stair[i], dp[i - 3] + stair[i - 1] + stair[i]);
+                }
+            }
         }
-
         System.out.println(dp[n]);
     }
 }
