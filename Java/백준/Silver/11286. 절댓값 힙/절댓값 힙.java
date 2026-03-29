bf556271be51a49ae8c578382a21f491fe3f436e
ricepark0709 <juncomputer0709@gmail.com>
2026-03-29 21:03:21 +0900

    [Silver I] Title: 절댓값 힙, Time: 292 ms, Memory: 26496 KB -BaekjoonHub

--- Java/백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ Java/백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -2,40 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb = new StringBuilder();
-    static PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2) -> {
-        int abs1 = Math.abs(o1);
-        int abs2 = Math.abs(o2);
-        
-        if (abs1 == abs2) {
-            return o1 - o2;
-        }
-        return abs1 - abs2;
-    });
-    
+
+    static int n;
+
+    static PriorityQueue<Long> plusQ = new PriorityQueue<>();
+    static PriorityQueue<Long> minusQ = new PriorityQueue<>(Collections.reverseOrder());
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        
-        for (int i = 0; i < n; i++) {
-            process(Integer.parseInt(br.readLine()));
-        }
-        printing();
-    }
-    
-    public static void process(int n) {
-        if (n != 0) q.offer(n);
-        else if (n == 0) {
-            if (q.isEmpty()) sb.append(0 + "\n");
-            else sb.append(q.poll() + "\n");
-        }
-    }
-    
-    public static void printing() throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+        fillQueue();
+
         bw.write(sb.toString());
-        
         bw.flush();
         bw.close();
+    }
+
+    public static void fillQueue() throws IOException {
+        for (int i = 0; i < n; i++) {
+            long num = Integer.parseInt(br.readLine());
+            if (num > 0) {
+                plusQ.add(num);
+            } else if (num < 0) {
+                minusQ.add(num);
+            } else {
+                if (plusQ.isEmpty() && !minusQ.isEmpty()) {
+                    sb.append(minusQ.poll()).append("\n");
+                } else if (!plusQ.isEmpty() && minusQ.isEmpty()) {
+                    sb.append(plusQ.poll()).append("\n");
+                } else if (plusQ.isEmpty() && minusQ.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    long temp1 = Math.abs(plusQ.peek());
+                    long temp2 = Math.abs(minusQ.peek());
+                    if (temp1 >= temp2) {
+                        sb.append(minusQ.poll()).append("\n");
+                    } else {
+                        sb.append(plusQ.poll()).append("\n");
+                    }
+                }
+            }
+        }
     }
 }
