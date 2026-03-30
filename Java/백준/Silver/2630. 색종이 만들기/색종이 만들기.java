29fa647f05070ad31366f75b1a72bc2672be73ab
ricepark0709 <juncomputer0709@gmail.com>
2026-03-31 06:51:55 +0900

    [Silver II] Title: 색종이 만들기, Time: 136 ms, Memory: 15600 KB -BaekjoonHub

--- Java/백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ Java/백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -2,54 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int white = 0;
-    static int blue = 0;
-    static int[][] board;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    
+
+    static int n, white, blue;
+
+    static int[][] arr;
+
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        board = new int[n][n];
-        
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        partition(0, 0, n);
-        
+        n = Integer.parseInt(br.readLine());
+        setArr();
+        recursion(0, 0, n);
+
         System.out.println(white);
         System.out.println(blue);
     }
-    
-    public static void partition(int r, int c, int size) {
-        if (checkColor(r, c, size)) {
-            if (board[r][c] == 0) white++;
+
+    public static void setArr() throws IOException {
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    public static void recursion(int x, int y, int size) {
+        if (check(x, y, size)) {
+            if (arr[x][y] == 0) white++;
             else blue++;
             return;
         }
-        
+
         int newSize = size / 2;
-        
-        partition(r, c, newSize);
-        partition(r, c + newSize, newSize);
-        partition(r + newSize, c, newSize);
-        partition(r + newSize, c + newSize, newSize);
+
+        recursion(x + newSize, y, newSize);
+        recursion(x, y + newSize, newSize);
+        recursion(x + newSize, y + newSize, newSize);
+        recursion(x, y, newSize);
     }
-    
-    public static boolean checkColor(int r, int c, int size) {
-        int color = board[r][c];
-        
-        for (int i = r; i < r + size; i++) {
-            for (int j = c; j < c + size; j++) {
-                if (board[i][j] != color) {
+
+    public static boolean check(int x, int y, int size) {
+        int color = arr[x][y];
+
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (arr[i][j] != color) {
                     return false;
                 }
             }
         }
+
         return true;
     }
 }
