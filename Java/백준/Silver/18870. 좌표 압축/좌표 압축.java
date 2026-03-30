f70f247a092405c187741a2cefc86d2093d61fab
ricepark0709 <juncomputer0709@gmail.com>
2026-03-30 14:05:40 +0900

    [Silver II] Title: 좌표 압축, Time: 1800 ms, Memory: 266044 KB -BaekjoonHub

--- Java/백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ Java/백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,38 +1,48 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] arr;
+    static int[] sorted;
+    static Map<Integer, Integer> map = new HashMap<>();
+
+    static int n, rank = 0;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        int[] origin = new int[N];    // 원본 배열
-        int[] sorted = new int[N];    // 정렬할 배열
-        HashMap<Integer, Integer> rankMap = new HashMap<>();
+        n = Integer.parseInt(br.readLine());
+        setArr();
+        process();
+    }
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < N; i++) {
-            origin[i] = sorted[i] = Integer.parseInt(st.nextToken());
+    public static void setArr() throws IOException {
+        arr = new int[n];
+        sorted = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = sorted[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(sorted);
 
-        // 2. 중복 제거하며 순위(index) 매기기
-        int rank = 0;
-        for(int v : sorted) {
-            if(!rankMap.containsKey(v)) {
-                rankMap.put(v, rank);
+        for (int i = 0; i < n; i++) {
+            if (!map.containsKey(sorted[i])) {
+                map.put(sorted[i], rank);
                 rank++;
             }
         }
+    }
 
-        // 3. 출력 최적화를 위해 StringBuilder 사용
-        StringBuilder sb = new StringBuilder();
-        for(int key : origin) {
-            int ranking = rankMap.get(key); // HashMap 탐색은 O(1)
-            sb.append(ranking).append(' ');
+    public static void process() throws IOException {
+        for (int i = 0; i < n; i++) {
+            sb.append(map.get(arr[i])).append(" ");
         }
-        
-        System.out.println(sb);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
