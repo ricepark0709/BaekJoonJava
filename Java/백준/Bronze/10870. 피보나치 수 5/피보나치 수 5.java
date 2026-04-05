b6b9631443962b7c8c962cd0504e1e803f35d2a3
ricepark0709 <juncomputer0709@gmail.com>
2026-04-06 00:17:05 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- Java/백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ Java/백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -2,29 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> s = new Stack<>(); // 원래 스택
-        Stack<Integer> s2 = new Stack<>(); // 대피시킬 스택
-        int n = Integer.parseInt(br.readLine());
+         n = Integer.parseInt(br.readLine());
+         setArr();
+         if (n > 1) {
+             fillArr(2);
+         }
+         System.out.println(arr[n]);
+    }
 
+    public static void setArr() {
+        arr = new int[n + 1];
+        arr[0] = 0;
+        if (n > 0) arr[1] = 1;
+    }
 
-        for (int i = 0; i < n + 3; i++) {
-            if (s.size() > 2) {
-                int newNum = 0;
-                for (int j = 0; j < 2; j++) {
-                    int a = s.pop();
-                    s2.push(a);
-                    newNum += a;
-                }
-                for (int k = 0; k < 2; k++) {
-                    s.push(s2.pop());
-                }
-                s.push(newNum);
-            } else if (s.size() == 1) s.push(1);
-            else s.push(0);
+    public static void fillArr(int index) {
+        if (index < n + 1) {
+            arr[index] = arr[index - 2] + arr[index - 1];
+            index++;
+            fillArr(index);
         }
-
-        System.out.println(s.pop());
     }
 }
